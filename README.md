32185d9d1690e55badb6573c69498f09c9898a49
Rubén <rubgij@gmail.com>
2025-02-24 11:34:18 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Task-manager-api
+# Task manager API
  Development of a Task Manager API using FastAPI. First Backend project.
+
+The goal is to develop an API for managing the tasks.
+
+## API Structure
+
+!https://raw.githubusercontent.com/rubzip/Task-manager-api/refs/heads/main/imgs/API_architecture.webp
+
+## DataBase Structure
+
+!https://raw.githubusercontent.com/rubzip/Task-manager-api/refs/heads/main/imgs/database.png
--- src/main.py
+++ src/main.py
@@ -1,8 +1,44 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+
+class Task(BaseModel):
+    task_id: int
+    title: str
+    description: str | None = None
+    user_id: int
+    working_on: bool = True
+    finished: bool = False
+    created_at: datetime | None = datetime.now()
+    deadline: datetime
+
+class User(BaseModel):
+    user_id: int
+    username: str
+    password: str
 
 app = FastAPI()
+
+
+@app.put("/users/{user_id}")
+async def create_task(user_id: int, new_task: Task):
+    result = new_task.model_dump()
+    result.update({"user_id": user_id})
+    return result
+
 
 
 @app.get("/")
 async def root() -> dict:
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
